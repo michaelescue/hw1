4824d063dd58bc1db7298630a00bf7c5d81dd9de
michael_escue <mescue@pdx.edu>
2018-10-06 13:49:17 -0700

    reset

--- hw1.c
+++ hw1.c
@@ -1,73 +1,92 @@
 #include <stdio.h>
 #include <math.h>
 
-int num1(float baselevel){
+/***************************************************
+Homework 1 source code.
+Author: Michael Escue
 
-float total = 0;
-float leaders_of_ten = 0;
-float leaders_of_one_hundered = 0;
-float leaders_of_one_thousand = 0;
+Date: 10/06/18
 
-total = baselevel;	//5763
+****************************************************/
 
-printf("total of base-level soldiers = %f\n", total);
+void div_ceil_acc(float *rank, float *return_total){
+	
+	*rank = (*rank/10.0);  
 
-leaders_of_ten = (baselevel/10.0);  // (5763/10) = 576.3
-printf("leaders of ten = %f", leaders_of_ten);
-printf("mod of ten is = %f", fmod(leaders_of_ten, 10));
+	*rank = ceil(*rank);	// Rounds up for fractional results. 
 
-leaders_of_ten = ceil(leaders_of_ten);
+	*return_total += (*rank);
 
-total += (leaders_of_ten);
-
-printf("total including \"leaders of ten\"= %f\n", total);
-
-leaders_of_one_hundered = (leaders_of_ten / 10);
-
-leaders_of_one_hundered = ceil(leaders_of_one_hundered);
-
-total += (leaders_of_one_hundered);
-
-printf("total including \"leaders of one-hundered\"= %f\n", total);
-
-leaders_of_one_thousand = (leaders_of_one_hundered / 10);
-
-leaders_of_one_thousand = ceil(leaders_of_one_thousand);
-
-total += (leaders_of_one_thousand);
-
-printf("total including \"leaders of one-thousand\"= %f\n", total);
-
-return 0;
 }
 
+//Function to check the math on question #1.
+int num1(float baselevel){
+	
+	float total = 0;						//Total count of soldiers.
+	float leaders_of_ten = 0;			
+	float leaders_of_one_hundered = 0;
+	float leaders_of_one_thousand = 0;
+
+	total = baselevel;						//Input given at the terminal initiates the total.
+
+	printf("total of base-level soldiers = %f\n", total);
+
+	div_ceil_acc(&leaders_of_ten, &total);
+	
+	/* leaders_of_ten = (baselevel/10.0);  
+
+	leaders_of_ten = ceil(leaders_of_ten);	// Rounds up for fractional results. 
+
+	total += (leaders_of_ten); */
+
+	printf("total including \"leaders of ten\"= %f\n", total);
+
+	div_ceil_acc(&leaders_of_one_hundered, &total);
+		
+	/* leaders_of_one_hundered = (leaders_of_ten / 10);
+
+	leaders_of_one_hundered = ceil(leaders_of_one_hundered);
+
+	total += (leaders_of_one_hundered); */
+
+	printf("total including \"leaders of one-hundered\"= %f\n", total);
+
+	div_ceil_acc(&leaders_of_one_thousand, &total);
+	
+	/* leaders_of_one_thousand = (leaders_of_one_hundered / 10);
+
+	leaders_of_one_thousand = ceil(leaders_of_one_thousand);
+
+	total += (leaders_of_one_thousand); */
+
+	printf("total including \"leaders of one-thousand\"= %f\n", total);
+
+	return 0;
+}
+
+
+// Main function. Contains function calls to assignment solutions.
 int main(void){
 
-int soldiers = 0;
+	int soldiers = 0;
+		
 
-while(1){
-printf("Problem #1:\n Enter the number of soldiers at the base level = ");
+	// Loop to test problem #1 with multiple values.
+	while(1){
+		printf("Problem #1:\n Enter the number of soldiers at the base level = ");
+		scanf("%d", &soldiers); getchar(); printf("\n");	//Same line for compact-ness.
 
-scanf("%d", &soldiers);
+		num1((float) soldiers);
 
-getchar();
+		printf("Done testing Problem #1? Enter \"y\" to exit, or any other character to continue: "); 
+		scanf("%c", &soldiers); getchar();
 
-printf("\n");
+		printf("\n");	// New line for while loop wrap around.
 
-num1((float) soldiers);
+		if(soldiers == 'y') break;
+		else soldiers = 0;
 
-printf("Done testing Problem #1? Enter \"y\" to exit: ");
-
-scanf("%c", &soldiers);
-
-getchar();
-
-printf("\n");
-
-if(soldiers == 'y') break;
-else soldiers = 0;
-
-}
+	}
 
 
 }
